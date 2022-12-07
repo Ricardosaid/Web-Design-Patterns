a1d23a9b73c8971ece1aa798261e8d9a7fc3fa99
SaidLeben <said_s027@hotmail.es>
2022-12-07 00:29:48 -0600

    first approach odd occurrencess

--- Codility/odd_occurrences_in_array/Program.cs
+++ Codility/odd_occurrences_in_array/Program.cs
@@ -1,17 +1,56 @@
 ﻿using System.Collections.Generic;
-int [] A = {9,3,9,3,9,7,9};
+int [] A = {0,0,7,1,7};
 var result = solution(A);
-
 static int solution(int [] A)
 {
     var count = 0;
-    var helper = new List<int>();
     var tam = A.Length;
-
-    for (int i = 1; i <= tam; i++)
+    int status = 0;
+    int helper=0;
+    
+    for (int i = 1; i < tam; i++)
     {
+        var estado = A[count];//9
+        //helper=estado;
+        if (A[i]==estado)//3=9 o 9==3
+        {
+            count++;
+            status = A[i];//statusdebe ser un numero
+        }
+        else if(status==estado)
+        {
+            count++;
+            //status=false;
+            //helper = A[i];
+        }
         
     }
 
-    return 0;
+    return helper;
 }
+
+
+
+/*
+foreach (var item in A)
+    {
+        var estado = A[count];
+        if (item == estado)
+        {
+            if(status)
+            {
+            count++;
+            //status = false;
+            }
+            else
+            {
+                status=true;
+            }
+        }
+        else
+        {
+            helper=item;
+            status = false;
+        }
+    }
+*/
