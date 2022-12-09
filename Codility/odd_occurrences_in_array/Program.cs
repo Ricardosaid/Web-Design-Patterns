ed03773dfdc8351d23f652c4df821e44ee5c8634
Ricardo Said Martinez <rsmartinez@bside.com.mx>
2022-12-09 00:11:34 -0600

    Second approach

--- Codility/odd_occurrences_in_array/Program.cs
+++ Codility/odd_occurrences_in_array/Program.cs
@@ -5,28 +5,72 @@ static int solution(int [] A)
 {
     var count = 0;
     var tam = A.Length;
-    int status = 0;
-    int helper=0;
-    
-    for (int i = 1; i < tam; i++)
-    {
-        var estado = A[count];//9
-        //helper=estado;
-        if (A[i]==estado)//3=9 o 9==3
-        {
-            count++;
-            status = A[i];//statusdebe ser un numero
-        }
-        else if(status==estado)
-        {
-            count++;
-            //status=false;
-            //helper = A[i];
-        }
-        
-    }
+    //var count_two = 1;
+    //var flag = false;
+    var helper = 0;
+    int[] numbers = {};
 
-    return helper;
+
+    while (tam!=1)
+    {
+        for (int i = 1; i < tam; i++)
+        {
+            var estado = A[count];
+            if (A[i]==estado)
+            {
+                helper = A[i];
+                i = tam-1;
+            }
+            else
+            {
+                count++;
+            }
+        }
+        numbers = A.Where(q=>q!=helper).ToArray();
+        
+        A=numbers;
+        tam = A.Length;
+        if (tam==1)
+        {
+            return (numbers[0]);
+        }
+    }
+    return (A[0]);
+    
+    // for (int i = 2; i < tam; i++)
+    // {
+    //     var estado = A[count_one];
+    //     //var helper = A[count_two];
+    //     if (A[i]==estado)
+    //     {
+    //         count_one++;
+    //         if (helper == A[i])
+    //         {
+
+                
+    //         }
+    //     }
+    //     else
+    //     {
+    //         helper = A[i];
+    //         flag = true;
+
+    //         // foreach (var item in B)
+    //         // {
+    //         //     if (helper==item)
+    //         //     {
+                    
+    //         //     }
+    //         //     else
+    //         //     {
+                    
+    //         //     }
+    //         // }
+    //     }
+        
+    // }
+
+
 }
 
 
