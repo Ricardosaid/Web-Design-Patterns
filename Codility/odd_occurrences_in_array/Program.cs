3e840ce297985113b001b6ffe512b318baea951d
Ricardo Said Martinez <rsmartinez@bside.com.mx>
2022-12-09 00:46:38 -0600

    fix first element in array test

--- Codility/odd_occurrences_in_array/Program.cs
+++ Codility/odd_occurrences_in_array/Program.cs
@@ -1,97 +1,39 @@
 ﻿using System.Collections.Generic;
-int [] A = {9,3,9,3,9,7,9};
+int[] A = {7, 0, 0, 1, 1,7,3};
 var result = solution(A);
 Console.WriteLine(result);
-static int solution(int [] A)
+static int solution(int[] A)
 {
     var count = 0;
     var tam = A.Length;
-    //var count_two = 1;
-    //var flag = false;
-    var helper = 0;
-    int[] numbers = {};
+    int? helper = null;//declarar entero nulo
+    int[] numbers = { };
+    bool flag = false;
 
-
-    while (tam!=1)
+    while (tam != 1)
     {
         for (int i = 1; i < tam; i++)
         {
             var estado = A[count];
-            if (A[i]==estado)
+            if (A[i] == estado)
             {
                 helper = A[i];
-                i = tam-1;
+                i = tam - 1;
+                flag = true;
+
             }
         }
-        numbers = A.Where(q=>q!=helper).ToArray();
-
-        A=numbers;
-        tam = A.Length;
-        if (tam==1)
+        if (flag)
         {
-            return (numbers[0]);
-        }
+            numbers = A.Where(q => q != helper).ToArray();
+            A = numbers;
+            tam = A.Length;
+            if (tam == 1)
+            {
+                return (numbers[0]);
+            }
+        } else return (A[0]);
+
     }
     return (A[0]);
-    
-    // for (int i = 2; i < tam; i++)
-    // {
-    //     var estado = A[count_one];
-    //     //var helper = A[count_two];
-    //     if (A[i]==estado)
-    //     {
-    //         count_one++;
-    //         if (helper == A[i])
-    //         {
-
-                
-    //         }
-    //     }
-    //     else
-    //     {
-    //         helper = A[i];
-    //         flag = true;
-
-    //         // foreach (var item in B)
-    //         // {
-    //         //     if (helper==item)
-    //         //     {
-                    
-    //         //     }
-    //         //     else
-    //         //     {
-                    
-    //         //     }
-    //         // }
-    //     }
-        
-    // }
-
-
 }
-
-
-
-/*
-foreach (var item in A)
-    {
-        var estado = A[count];
-        if (item == estado)
-        {
-            if(status)
-            {
-            count++;
-            //status = false;
-            }
-            else
-            {
-                status=true;
-            }
-        }
-        else
-        {
-            helper=item;
-            status = false;
-        }
-    }
-*/
