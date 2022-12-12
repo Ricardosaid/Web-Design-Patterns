a48d5609fb8931d64e1dfd22f3c1b2d9bddf053d
SaidLeben <said_s027@hotmail.es>
2022-12-12 15:52:37 -0600

    approach but with time complexity

--- Codility/odd_occurrences_in_array/Program.cs
+++ Codility/odd_occurrences_in_array/Program.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
-int[] A = {1, 3, 5, 9, 17, 17,9,5,1,1,1,3};
+int[] A = {9,9,2,3,2,4,5,3,5,4,3};
 var result = solution(A);
 Console.WriteLine(result);
 static int solution(int[] A)
 {
     var count = 0;
     var tam = A.Length;
-    int? helper = null;//declarar entero nulo
-    int[] numbers = { };
+    int helper = 0;//declarar entero nulo
+    //List<int> numbers = new List<int>();
+    int[] B = {};
     bool flag = false;
 
     while (tam != 1)
@@ -17,17 +18,26 @@ static int solution(int[] A)
             var estado = A[count];
             if (A[i] == estado)
             {
-                helper = A[i];
+                helper = i;
                 i = tam - 1;
                 flag = true;
 
             }
+
+
         }
         if (flag)
         {
             flag = false;
-            numbers = A.Where(q => q != helper).ToArray();
-            A = numbers;
+            A = A.Where((val,idx)=>idx!=helper).ToArray();
+            A = A.Where((val,idx)=>idx!=count).ToArray();
+            //A=B;
+            /*
+            var numbers = A.ToList();
+            numbers.RemoveAt(helper);
+            numbers.RemoveAt(count);
+            */
+            //A = numbers.ToArray();
             tam = A.Length;
             if (tam == 1)
             {
