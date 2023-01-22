4378bb3e9493538a153603146299aac613bddc73
saidleben <said_s027@hotmail.es>
2023-01-21 22:28:19 -0600

    update gitignore

--- Codility/odd_occurrences_in_array/Program.cs
+++ Codility/odd_occurrences_in_array/Program.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections.Generic;
- //int[] A = {9,9,2,3,2,4,5,3,5,4,3};
+﻿//int[] A = {9,9,2,3,2,4,5,3,5,4,3};
 // int[] A = {1004, 1002, 1005, 1004,1003, 1001, 1002,1002, 1002, 1000,1001, 1000, 1003, 1000, 1002, 1002, 1003, 1003, 1005, 1005, 1002, 1000, 1000, 1003, 1002, 1002, 1002, 1000, 1004, 1003, 1003, 1004, 1004, 1004, 1002, 1000, 1005, 1000, 1002, 1000, 1003, 1002, 1001, 1003, 1000, 1004, 1004, 1004, 1001, 1001, 1002, 1002, 1001, 1003, 1005, 1005, 1002, 1002, 1004, 1003, 1001, 1004, 1003, 1000, 1004, 1002, 1004, 1004, 1005, 1005, 1002, 1001, 1003, 1005, 1003, 1001, 1000, 1005, 1001, 1004, 1000, 1001, 1004, 1004, 1003, 1005, 1000, 1004, 1005, 1002, 1003, 1004, 1005, 1001, 1004, 1001, 1001, 1001, 1005, 1005, 1001, 1004, 1005};
-int[] A = {3,3,3,3,3,3,3,3,3,3,6};
+//int[] A = {3,3,3,3,3,3,3,3,3,3,6};
+
+int[] A = { 9, 3, 9, 3, 9, 7, 9 };
 var result = solution(A);
 Console.WriteLine(result);
 static int solution(int[] A)
@@ -9,47 +10,42 @@ static int solution(int[] A)
     var count = 0;
     var tam = A.Length;
     int helper = 0;
-    int estado = 0;
+    int estado = 0; 
     int estado2 = 0;
 
     if (tam==1) return (A[0]);
 
-    while(helper==0)
+    do
     {
         for (int i = 0; i < tam; i++)
         {
-            if (count==i) continue;
-            else
-            {
-                if (A[count]==A[i])
-                {
-                    helper++;
-                    estado = i;
-                }
-                else{
-                    estado2 = count;
-                }
-            }
             
+            if (count==i) continue;
+            if (A[count]==A[i])
+            {
+                helper++;
+                estado = i;
+            }
+            else estado2 = count;
         }
 
         if (helper == 0)
         {
-           Console.WriteLine(estado2);
+            Console.WriteLine(estado2);
             return(A[estado2]); 
         }
 
-         else if(helper%2 == 0)
+        if(helper%2 == 0)
         {
             Console.WriteLine(estado);
             return (A[estado]);
         }
 
-        else{
-            count++;
-            helper = 0;     
-        }
-    }
+        count++;
+        helper = 0;
+        
+    } while (helper==0);
+    
     return (A[0]);
     
     // for (int i = 1; i < tam; i++)
