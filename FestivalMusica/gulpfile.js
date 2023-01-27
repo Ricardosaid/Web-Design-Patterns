83df0bbdb4af1dbe3a4e4070555b8ecfbe31604e
SaidLeben <said_s027@hotmail.es>
2023-01-27 01:38:26 -0600

    added optimization gulp images

--- FestivalMusica/gulpfile.js
+++ FestivalMusica/gulpfile.js
@@ -6,15 +6,10 @@ const sass = require("gulp-sass")(require('sass'));
 const plumber = require("gulp-plumber");
 
 // Dependencias de imagenes
+const cache = require("gulp-cache");
+const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
 
-function tarea(cb) {
-  console.log("First task");
-  cb();
-}
-
-//Mandar llamar una funcion con node
-exports.primerTarea = tarea; // cuando mande llamar primer tarea, va a ejecutar la funcion 'tarea'
 
 //Gulp nos da una api que busca, compila y guarda
 
@@ -25,6 +20,16 @@ function css(done) {
   .pipe(dest("build/css")); //Almacenarla o guardarla en el disco duro
 
   done(); // callback que avisa a gulp cuando llegamos al final de la funcion
+}
+
+function imagenes(done){
+  const opciones = {
+    optimizationLevel : 3
+  }
+  src("src/img/**/*.{png,jpg}")
+  .pipe(cache(imagemin(opciones)))
+  .pipe(dest("build/img"))
+  done();
 }
 
 function versionWebp(done){
@@ -45,4 +50,5 @@ function dev(done) {
 
 exports.css = css;
 exports.version = versionWebp;
-exports.watchDev = parallel(versionWebp,dev);
+exports.imagenes = imagenes;
+exports.watchDev = parallel(imagenes,versionWebp,dev);
